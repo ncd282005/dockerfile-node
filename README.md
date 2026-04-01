ef40a48674fba9ea788ee965be65342d0c7b6966
Your Name <your_email@gmail.com>
2026-04-01 17:12:56 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# dockerfile-node
+# dockerfile-node
+# dockerfile-node
---
+++ app.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+
+const port = 3000;
+
+const username = "system";
+const password = process.env.PASSWORD;
+const secretMessage = process.env.SECRET_MESSAGE;
+
+// Basic Auth middleware
+function auth(req, res, next) {
+  const authHeader = req.headers['authorization'];
+
+  if (!authHeader) {
+    res.set('WWW-Authenticate', 'Basic');
+    return res.status(401).send('Authentication required');
+  }
+
+  const [scheme, encoded] = authHeader.split(' ');
+  const [user, pass] = Buffer.from(encoded, 'base64').toString().split(':');
+
+  if (user === username && pass === password) {
+    return next();
+  }
+
+  return res.status(403).send('Invalid credentials');
+}
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.get('/secret', auth, (req, res) => {
+  res.send(secretMessage);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
